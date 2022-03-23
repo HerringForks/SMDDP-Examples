cc148d3068726d24f538d8b239d88af05ed49e39
sitecao <sitecao@amazon.com>
2022-03-23 23:11:44 +0000

    remove barrier device_ids argument

--- pytorch/object_detection/yolov5/utils/torch_utils.py
+++ pytorch/object_detection/yolov5/utils/torch_utils.py
@@ -32,10 +32,12 @@ def torch_distributed_zero_first(local_rank: int):
     Decorator to make all processes in distributed training wait for each local_master to do something.
     """
     if local_rank not in [-1, 0]:
-        dist.barrier(device_ids=[local_rank])
+        #dist.barrier(device_ids=[local_rank])
+        dist.barrier()
     yield
     if local_rank == 0:
-        dist.barrier(device_ids=[0])
+        #dist.barrier(device_ids=[0])
+        dist.barrier()
 
 
 def date_modified(path=__file__):
