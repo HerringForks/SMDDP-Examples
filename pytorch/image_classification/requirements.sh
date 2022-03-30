aba9076d73856c0d62ccaa06f2b6341882a0c7de
carolynwang <carowang@amazon.com>
2022-03-30 13:08:31 -0700

    update dllogger install

--- pytorch/image_classification/requirements.sh
+++ pytorch/image_classification/requirements.sh
@@ -1,3 +1,3 @@
-pip install git+git://github.com/NVIDIA/dllogger.git@26a0f8f1958de2c0c460925ff6102a4d2486d6cc#egg=dllogger
+pip install 'git+https://github.com/NVIDIA/dllogger'
 pip install nvidia-pyindex
 pip install pytorch-quantization
